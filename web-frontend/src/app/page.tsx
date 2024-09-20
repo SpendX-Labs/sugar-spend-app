03f88104774c8781392d2e790fda90203902ea20
Manoj-K-Sarkar <wakeupmanoj123@gmail.com>
2024-09-20 10:47:36 +0530

    feat: add shadcn components

--- web-frontend/src/app/page.tsx
+++ web-frontend/src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
             Deploy now
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid  transition-colors flex items-center justify-center   text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
