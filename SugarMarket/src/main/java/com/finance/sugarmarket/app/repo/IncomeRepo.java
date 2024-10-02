5762ba00e8182a2a2714b0fa0460402fdff449d1
subham.biswas <subhambiswas0028@gmail.com>
2024-10-02 08:09:10 +0530

    get total income fix

--- SugarMarket/src/main/java/com/finance/sugarmarket/app/repo/IncomeRepo.java
+++ SugarMarket/src/main/java/com/finance/sugarmarket/app/repo/IncomeRepo.java
@@ -10,10 +10,10 @@ import com.finance.sugarmarket.app.model.Income;
 
 public interface IncomeRepo extends JpaRepository<Income, Long>, JpaSpecificationExecutor<Income> {
 
-	@Query("SELECT SUM(i.amount) FROM Income i WHERE i.user.id = :userId AND YEAR(i.dateOfEvent) = :year AND MONTHNAME(i.dateOfEvent) = :month")
+	@Query("SELECT COALESCE(SUM(i.amount), 0) FROM Income i WHERE i.user.id = :userId AND YEAR(i.dateOfEvent) = :year AND MONTHNAME(i.dateOfEvent) = :month")
 	BigDecimal getTotalMonthlyIncome(Integer year, String month, Long userId);
 
-	@Query("SELECT SUM(i.amount) FROM Income i WHERE i.user.id = :userId AND YEAR(i.dateOfEvent) = :year")
+	@Query("SELECT COALESCE(SUM(i.amount), 0) FROM Income i WHERE i.user.id = :userId AND YEAR(i.dateOfEvent) = :year")
 	BigDecimal getTotalYearlyIncome(Integer year, Long userId);
 
 }
