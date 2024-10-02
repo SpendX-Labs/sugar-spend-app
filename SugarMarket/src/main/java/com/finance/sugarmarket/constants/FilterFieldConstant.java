bcebf93d7e4225ff3b78ca2395d3dd139fd0befc
subham.biswas <subhambiswas0028@gmail.com>
2024-10-02 07:25:50 +0530

    rename to limit and offset

--- SugarMarket/src/main/java/com/finance/sugarmarket/constants/FilterFieldConstant.java
+++ SugarMarket/src/main/java/com/finance/sugarmarket/constants/FilterFieldConstant.java
@@ -1,8 +1,8 @@
 package com.finance.sugarmarket.constants;
 
 public class FilterFieldConstant {
-	public static String PAGE = "page";
-	public static String SIZE = "size";
+	public static String OFFSET = "offset";
+	public static String LIMIT = "limit";
 	public static String ORDER_BY = "orderBy";
 	public static String SORT_COLUMN = "sortColumn";
 	public static String ID = "id";
